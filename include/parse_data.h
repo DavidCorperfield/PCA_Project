c8edcda06d2df388f6ea78255ee0dc7758023788
colin <colin@colinpc.(none)>
2013-03-17 18:16:57 -0400

    finished the data parser and added print functionality for images/labels

--- include/parse_data.h
+++ include/parse_data.h
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdint.h>
+#include <assert.h>
+ 
+//data directory with respect to the binary
 #define data_dir "./data/"
 
-char * get_data(char *filename);
+int num_items = 0;
+int num_images = 0;
+
+uint8_t *get_data(char *filename);
+void print_example(int start_index, uint8_t* images, uint8_t *labels);
