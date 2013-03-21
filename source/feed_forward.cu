9e07029b1ef77d422b970a777f2211c5f6e1faff
colin <colin@colinpc.(none)>
2013-03-21 14:49:48 -0400

    added feed forward source

---
+++ source/feed_forward.cu
@@ -0,0 +1,16 @@
+#include "../inlcude/feed_forward.h"
+#include <helper_cuda.h>
+#include <helper_functions.h>
+
+
+__global__ void 
+eval_layer(int num_neurons, float *input, float *weights[], float *output)
+{
+    int blockID = blockIdx.x;
+    int pixel1x
+
+    float result = 0;
+    
+
+}
+
